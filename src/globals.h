bcf9db7510038d2da0a10ebe2f02da959ad0b09b
Dhruv <dhruvsha0507@gmail.com>
2025-03-07 14:50:55 +1100

    Add comments

--- src/globals.h
+++ src/globals.h
@@ -10,7 +10,10 @@ std::string input(const char* message);
 std::string to_hex(const unsigned char* ciphertext, size_t length);
 size_t un_hex(std::string hex_string, unsigned char* plaintext, size_t length);
 
+// I have no idea why this function exists
 void print_array(unsigned char* array, size_t length);
+
+// Copy text to clipboard
 void copy(const byte_string& text);
 
 void disable_echo();
